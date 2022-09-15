5bb70c1d241ee71dd6f5edd458b46901d621aa03
saafaaari <111dltldnjs@naver.com>
2022-09-16 01:05:44 +0900

    refactor: MainListViewDependencies 이동

--- TodoApp/ProjectManager/Presentation/MainList/ContentList/View/ContentListViewController.swift
+++ TodoApp/ProjectManager/Presentation/MainList/ContentList/View/ContentListViewController.swift
@@ -9,6 +9,13 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
+protocol MainListViewDependencies: AnyObject {
+    func presentEditViewController(item: TodoModel?)
+    func popoverMoveViewController(cell: UITableViewCell?, item: TodoModel)
+    func popoverHistoryViewController(button: UIBarButtonItem)
+    func showErrorAlert(message: String)
+}
+
 final class ContentListViewController: UIViewController {
     private let mainView = ContentListView()
     private let viewModel: ContentListViewModel
